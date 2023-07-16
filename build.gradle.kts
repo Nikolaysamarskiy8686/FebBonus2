94a5ab9130ab9f1a7624d565a5c41db1f1b5b38f
Kolya <kolya.samarskiy.2015@mail.ru>
2023-07-16 13:26:26 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+}
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+group = "ru.netology"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    useJUnit()
+    useTestNG()
+}
